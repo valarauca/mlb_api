6154c3d5ad7f3652857175f4fa79a3deb4f2e2b3
William Laeder <codylaeder@gmail.com>
2016-06-18 21:09:29 -0400

    Added another method

--- src/lib.rs
+++ src/lib.rs
@@ -66,3 +66,45 @@ pub fn player_profile_json(
         Err(e) => return Err(RequestError::Json(e))
     }
 }
+
+//get injury list
+pub fn injury_list_json(
+    client: &mut Client,
+    access_level: &str,
+    version: &str,
+    api_key: &str ) -> Result< Json, RequestError> {
+
+    //make a empty string to hold url
+    let mut url = String::with_capacity(4000);
+    //construct url
+    url.push_str("https://api.sportradar.us/mlb-");
+    url.push_str(access_level);
+    url.push_str(version);
+    url.push_str("/league/injuries.json?api_key=");
+    url.push_str(api_key);
+
+    //make request
+    let mut response = match client.get(&url).send() {
+        Ok(x) => x,
+        Err(e) => return Err(RequestError::Get(e))
+    };
+
+    //check response code
+    match response.status {
+        hyper::status::StatusCode::Ok => { },
+        x => return Err(RequestError::Status(x))
+    };
+
+    //read body
+    let mut read_buffer = String::with_capacity(8000);
+    match response.read_to_string( &mut read_buffer ) {
+        Ok(_) => { },
+        Err(e) => return Err(RequestError::Read(e))
+    };
+
+    //convert to json (and return)
+    match Json::from_str( &read_buffer ) {
+        Ok(x) => Ok(x),
+        Err(e) => return Err(RequestError::Json(e))
+    }
+}
